1bef2283edc568d90d298726a51dfe2c586edff2
aematei <alexanderematei@gmail.com>
2024-07-19 17:17:19 -0700

    Create search bar and topic buttons
    Signed-off-by: aematei <alexanderematei@gmail.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,45 @@
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  Button,
+  ButtonText,
+  ButtonSpinner,
+  ButtonIcon,
+  ButtonGroup,
+} from "@/components/ui/button";
+
+import { HStack } from "@/components/ui/hstack";
+import { Icon, SearchIcon } from "@/components/ui/icon";
+import { Input, InputField } from "@/components/ui/input";
+import { StyleSheet, View, Text } from "react-native";
+
+const BUTTON_SIZE = "sm"
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-lg text-blue-700">Hello world!</Text>
+    <View className="flex-1 items-center pt-8 px-6">
+      <Input
+        variant="outline"
+        size="md"
+        isDisabled={false}
+        isInvalid={false}
+        isReadOnly={false}
+      >
+        <Icon as={SearchIcon} className="text-black m-2 w-6 h-6" />
+        <InputField placeholder="Search here..." />
+      </Input>
+        <ButtonGroup className="pt-6">
+          <Button size={BUTTON_SIZE} variant="outline" action="primary">
+            <ButtonText>Breaking</ButtonText>
+          </Button>
+          <Button size={BUTTON_SIZE} variant="outline" action="primary">
+            <ButtonText>Local</ButtonText>
+          </Button>
+          <Button size={BUTTON_SIZE} variant="outline" action="primary">
+            <ButtonText>Sports</ButtonText>
+          </Button>
+          <Button size={BUTTON_SIZE} variant="outline" action="primary">
+            <ButtonText>Food</ButtonText>
+          </Button>
+        </ButtonGroup>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1, 
-    justifyContent: 'center',
-  }
-});
