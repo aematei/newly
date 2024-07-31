9de01cf05f13deea1722084133532544600a23da
aematei <alexanderematei@gmail.com>
2024-07-31 15:46:40 -0700

    Create tabs bar and add home page to it
    Signed-off-by: aematei <alexanderematei@gmail.com>

---
+++ archive/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import {
+  Button,
+  ButtonText,
+  ButtonSpinner,
+  ButtonIcon,
+  ButtonGroup,
+} from "@/components/ui/button";
+import { HStack } from "@/components/ui/hstack";
+import { Icon, SearchIcon } from "@/components/ui/icon";
+import { Input, InputField } from "@/components/ui/input";
+import { StyleSheet, View, Text } from "react-native";
+
+const BUTTON_SIZE = "sm";
+
+export default function Tab() {
+  return (
+    <View className="flex-1 items-center pt-8 px-6">
+      <Text></Text>
+    </View>
+  );
+}
+
